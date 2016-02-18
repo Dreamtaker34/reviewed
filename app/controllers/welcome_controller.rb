a842d84a154243f7d9973e79ce78fce3cc2d4abd
Dreamtaker34 <dreamtaker@gmail.com>
2016-02-18 18:33:34 -0500

    Tmdb API basic search added to front page.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,15 @@
 class WelcomeController < ApplicationController
   def index
+
   end
+
+  def search
+
+    Tmdb::Api.key(ENV["movie_db_key"])
+    @movies = Tmdb::Movie.find(params[:q])
+
+    render :index
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
+  # resources :welcome
+
+  get 'welcome/search' => 'welcome#search'
+
   get 'welcome/index'
 
   root 'welcome#index'
