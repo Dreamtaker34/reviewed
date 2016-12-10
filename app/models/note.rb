86632f9d5d88923740ef72b08d036e28a460b7c7
Dreamtaker34 <dreamtaker@gmail.com>
2016-12-10 02:28:15 -0500

    Change personal note from 1000 to 10000.

--- app/models/note.rb
+++ app/models/note.rb
@@ -23,7 +23,7 @@ class Note < ActiveRecord::Base
 
   accepts_nested_attributes_for :movie, :tvshow
 
-  validates :personal, length: { maximum: 1000 }
+  validates :personal, length: { maximum: 10000 }
   validates :shared, length: { maximum: 300 }
   validates :progress, length: { maximum: 40 }
 
